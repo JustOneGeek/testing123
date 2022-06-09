dec8587ff08662357afba350cd5cb3ed6b284342
kevin beier <kevinb@codingtemple.com>
2022-06-09 14:18:08 -0400

    deploy test

--- client/src/api/apiStripe.js
+++ client/src/api/apiStripe.js
@@ -4,8 +4,8 @@ const endpoint='/api/create-checkout-session'
 
 export const postTransaction=async(token, data, cancelToken)=>{
     const response = await apiClient(token,cancelToken).post(endpoint, data);
-    console.log('here', response, data)
-    return 
+
+    return window.location.href=response.data.url
 }
 
 export default{
